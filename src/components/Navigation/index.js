3f30bc8118b67761b956899c4b21f2c67390fc53
hugh-bowie <hughbowie@me.com>
2021-08-14 13:03:28 -0400

    SCSS fix

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 function Navigation(props) {
 	return (
 		<div className=' row navigation' id='navigation'>
-			<NavLink to='/portfolio'>Portfolio </NavLink>
-			<NavLink to='/about'>About </NavLink>
-			<NavLink to='/contact'>Contact </NavLink>
+			<NavLink to='/portfolio'>Portfolio</NavLink>
+			<NavLink to='/about'>About</NavLink>
+			<NavLink to='/contact'>Contact</NavLink>
 			<NavLink to='/resume'>Resume</NavLink>
 		</div>
 	);
